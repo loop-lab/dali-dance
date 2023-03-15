450ab96db82eb53e29374ed17d81883220a53919
Владислав Ежов <v.ezhov@qsoft.ru>
2023-03-15 18:50:03 +0300

    add column for count lessons

--- app/Orchid/Layouts/Customer/CustomerListLayout.php
+++ app/Orchid/Layouts/Customer/CustomerListLayout.php
@@ -2,8 +2,10 @@
 
 namespace App\Orchid\Layouts\Customer;
 
+use Carbon\Carbon;
 use Orchid\Screen\TD;
 use App\Models\Customer;
+use App\Models\Ticket;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Layouts\Table;
@@ -55,6 +57,18 @@ class CustomerListLayout extends Table
                 ->cantHide()
                 ->render(function (Customer $customer) {
                     return Link::make($customer->bonuses)
+                        ->route('platform.customers.edit', $customer->id);
+                }),
+
+            TD::make('count_lessons', 'Количество занятий')
+                ->sort()
+                ->cantHide()
+                ->render(function (Customer $customer) {
+                    $now = Carbon::now()->format('Y-m-d');
+                    $ticket_id = Ticket::where('customer_id', $customer->id)
+                        ->where('start_date', '<', $now)->where('stop_date', '>', $now)->first() ?? null;
+                    $count = $customer->lessons->where('ticket_id', $ticket_id)->count();
+                    return Link::make($count)
                         ->route('platform.customers.edit', $customer->id);
                 }),
 
